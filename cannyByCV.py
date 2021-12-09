57f0193c6ecc308b2273b2f54d17c6265829844f
BowenDeng <2631702650@qq.com>
2021-12-09 11:23:10 +0800

    init

--- cannyByCV.py
+++ cannyByCV.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2
 
-image = cv2.imread("D:\\Work-Coding\\_Projects\\PycharmProject\\ACannyAchieve\\Pic1_1.bmp")  # 读入图像
+image = cv2.imread("D:\\Work-Coding\\_Projects\\PycharmProject\\ACannyAchieve\\Pic1_3.bmp")  # 读入图像
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # 将图像转化为灰度图像
 cv2.imshow("Image", image)  # 显示图像
 # Canny边缘检测
-canny = cv2.Canny(image, 30, 150)
+canny = cv2.Canny(image, 90, 90)
 cv2.imshow("Canny", canny)
 cv2.waitKey()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
-import canny
+import cannyByDBW
 
 image = cv2.imread("D:\\Work-Coding\\_Projects\\PycharmProject\\ACannyAchieve\\Pic1_1.bmp")
 cv2.imshow("src", image)
-result = canny.Canny(image, 85, 95)
+result = cannyByDBW.Canny(image, 85, 95)
 cv2.imshow("dst", result)
 cv2.waitKey(0)
